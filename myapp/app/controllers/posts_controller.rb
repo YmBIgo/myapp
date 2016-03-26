96d64687be18c48e529b0560b35429914bd1bf2f
Kazuya <IgYmB0218LP@gmail.com>
2016-03-26 11:36:03 +0900

    page5-1-6

--- myapp/app/controllers/posts_controller.rb
+++ myapp/app/controllers/posts_controller.rb
@@ -21,6 +21,8 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:content, :place))
   end
 
   def destroy
