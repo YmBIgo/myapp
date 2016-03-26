6a96a2ba788b6a344179b43512e58b33765c182f
Kazuya <IgYmB0218LP@gmail.com>
2016-03-26 11:18:16 +0900

    page4-1-3

--- myapp/db/schema.rb
+++ myapp/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160326021046) do
+ActiveRecord::Schema.define(version: 20160326021656) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name",       limit: 255
     t.text     "content",    limit: 65535
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "place",      limit: 255
   end
 
 end
